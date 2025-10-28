da506d476464926c501c9ccb41b22a71b03e3e3e
anilkumar678 <aniladityajava@gmail.com>
2025-10-28 17:03:36 +0530

    childTest2 added in child1 branch

---
+++ src/main/java/org/example/ChildTest2.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import java.net.StandardSocketOptions;
+
+public class ChildTest2 {
+    public static void main(String args[]){
+       System.out.print("ChildTest2 changes");
+    }
+}
