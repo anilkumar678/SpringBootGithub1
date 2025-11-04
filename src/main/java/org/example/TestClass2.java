4a0eb43b79f877a9f30dce40bec33dde80787709
anilkumar678 <aniladityajava@gmail.com>
2025-11-04 11:44:47 +0530

    Update TestClass2.java changes in github

--- src/main/java/org/example/TestClass2.java
+++ src/main/java/org/example/TestClass2.java
@@ -3,6 +3,7 @@ package org.example;
 public class TestClass2 {
     public static void main(String args[]){
         System.out.println("child2 package");
+         System.out.println("child3 package added in github");
 
     }
 }
